18a8bdeda9fc0be908c3dc4b96fcc4cfb5556762
LivetaK <159531709+LivetaK@users.noreply.github.com>
2024-03-17 23:34:57 +0200

    Update studentai.cpp

--- studentai.cpp
+++ studentai.cpp
@@ -1,6 +1,7 @@
 #include "studentai.h"
 
-void ivedimas (vector<mok>& stud) {
+
+void ivedimas(vector<mok>& stud) {
     mok naujas_stud;
     cout << "Iveskite studento varda ir pavarde:" << endl;
     cin >> naujas_stud.var >> naujas_stud.pav;
@@ -16,7 +17,7 @@ void ivedimas (vector<mok>& stud) {
         }
 
         if (pazymys < 0) {
-                break;
+            break;
         }
         naujas_stud.nd.push_back(pazymys);
     }
@@ -24,23 +25,24 @@ void ivedimas (vector<mok>& stud) {
         if (naujas_stud.nd.empty()) {
             throw runtime_error("Neivestas nei vienas pazymys.");
         }
-    } catch (const runtime_error& e) {
+    }
+    catch (const runtime_error& e) {
         cout << e.what() << endl;
         exit(1);
     }
     cout << "Iveskite studento egzamino rezultata:" << endl;
-    while (!(cin >> naujas_stud.eg) || naujas_stud.eg < 1 || naujas_stud.eg > 10){
+    while (!(cin >> naujas_stud.eg) || naujas_stud.eg < 1 || naujas_stud.eg > 10) {
         cout << "Netinkamas formatas, bandykite dar karta" << endl;
         cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
-   
+
     stud.push_back(naujas_stud);
 }
 
-void meniu (){
+void meniu() {
     cout << "Pasirinkite" << endl;
-    cout << "1. Ivesti studentu duomenis ranka."<< endl;
+    cout << "1. Ivesti studentu duomenis ranka." << endl;
     cout << "2. Pazymius sugeneruoti automatiskai." << endl;
     cout << "3. Automatiskai sugeneruoti ir pazymius, ir studentu vardus, pavardes." << endl;
     cout << "4. Baigti darba." << endl;
@@ -55,118 +57,185 @@ void calculateResults(vector<mok>& stud) {
         sort(stud[i].nd.begin(), stud[i].nd.end());
         if (stud[i].nd.size() % 2 == 0) {
             stud[i].gal_med = ((stud[i].nd[stud[i].nd.size() / 2 - 1] + stud[i].nd[stud[i].nd.size() / 2]) / 2.0 * 0.4) + 0.6 * stud[i].eg;
-        } else {
+        }
+        else {
             stud[i].gal_med = (stud[i].nd[stud[i].nd.size() / 2] * 0.4) + (0.6 * stud[i].eg);
         }
     }
 }
 
-char rikiavimoklausimas(){
+char rikiavimoklausimas() {
     cout << "Jei norite matyti galutini rezultata apskaiciuota pagal:" << endl;
     cout << "vidurki - iveskite V" << endl;
-    cout << "mediana - iveskite M"<< endl;
+    cout << "mediana - iveskite M" << endl;
     char a;
-    while (!(cin >> a) || (a != 'V' && a != 'v' && a != 'M' && a != 'm' )){
+    while (!(cin >> a) || (a != 'V' && a != 'v' && a != 'M' && a != 'm')) {
         cout << "Netinkama ivestis, bandykite dar karta" << endl;
         cin.clear();
         cin.ignore();
     }
     return a;
 }
-void isvedimas(vector<mok>& stud, ostream& os, char a){
-    
-    if (a == 'V' || a == 'v'){
-            os << left << setw(20) <<"Pavarde" << setw(20) << "Vardas" << setw(20) << "Galutinis (Vid.)" << endl;
-            os << "--------------------------------------------------------------" << endl;
-            for (int i = 0; i < stud.size(); i++){
-            os << left << setw(20) << stud[i].var << setw(20)<< stud[i].pav << setw(20) << fixed << setprecision(2) << stud[i].gal_vid << endl;
-        }
-    } if ( a== 'M' || a == 'm'){
-            os << left << setw(20) <<"Pavarde" << setw(20) << "Vardas" << setw(20) <<  "Galutinis (Med.)" << endl;
-            os << "--------------------------------------------------------------" << endl;
-            for (int i = 0; i < stud.size(); i++){
-            os << left << setw(20) << stud[i].var << setw(20)<< stud[i].pav << setw(20) << fixed << setprecision(2) << stud[i].gal_med << endl;
-        }
-        }
-        }
+void isvedimas(vector<mok>& stud, ostream& os, char a) {
 
-void meniuAntras(int& antrasPasirinkimas){
+    if (a == 'V' || a == 'v') {
+        os << left << setw(20) << "Pavarde" << setw(20) << "Vardas" << setw(20) << "Galutinis (Vid.)" << endl;
+        os << "--------------------------------------------------------------" << endl;
+        for (int i = 0; i < stud.size(); i++) {
+            os << left << setw(20) << stud[i].var << setw(20) << stud[i].pav << setw(20) << fixed << setprecision(2) << stud[i].gal_vid << endl;
+        }
+    } if (a == 'M' || a == 'm') {
+        os << left << setw(20) << "Pavarde" << setw(20) << "Vardas" << setw(20) << "Galutinis (Med.)" << endl;
+        os << "--------------------------------------------------------------" << endl;
+        for (int i = 0; i < stud.size(); i++) {
+            os << left << setw(20) << stud[i].var << setw(20) << stud[i].pav << setw(20) << fixed << setprecision(2) << stud[i].gal_med << endl;
+        }
+    }
+}
+
+void meniuAntras(int& antrasPasirinkimas) {
     cout << "jei norite, kad duomenys butu isrikiuoti pagal:" << endl;
     cout << "varda - iveskite 1" << endl;
     cout << "pavarde - iveskite 2" << endl;
     cout << "vidurki - iveskite 3" << endl;
     cout << "mediana - iveskite 4" << endl;
-    while (!(cin >> antrasPasirinkimas) || antrasPasirinkimas < 1 || antrasPasirinkimas > 4){
+    while (!(cin >> antrasPasirinkimas) || antrasPasirinkimas < 1 || antrasPasirinkimas > 4) {
         cout << "Neteisingas pasirinkimas. Bandykite dar karta." << endl;
         cin.clear();
         cin.ignore(numeric_limits<std::streamsize>::max(), '\n');
     }
-    }
+}
 
-bool pagalVarda(const mok& a, const mok& b){
-    return stoi(a.var.substr(6,1))< stoi(b.var.substr(6,1));
+bool pagalVarda(const mok& a, const mok& b) {
+    return stoi(a.var.substr(6, 1)) < stoi(b.var.substr(6, 1));
 }
-bool pagalPavarde(const mok& a, const mok& b){
-    return stoi(a.pav.substr(7,1)) < stoi (b.pav.substr(7,1));
+bool pagalPavarde(const mok& a, const mok& b) {
+    return stoi(a.pav.substr(7, 1)) < stoi(b.pav.substr(7, 1));
 }
-bool pagalMediana(const mok& a, const mok& b){
+bool pagalMediana(const mok& a, const mok& b) {
     return a.gal_med < b.gal_med;
 }
-bool pagalVidurki(const mok& a, const mok& b){
+bool pagalVidurki(const mok& a, const mok& b) {
     return a.gal_vid < b.gal_vid;
 }
 
-milliseconds trukmesSkaiciavimas(high_resolution_clock::time_point pradzia,high_resolution_clock::time_point pabaiga) {
+milliseconds trukmesSkaiciavimas(high_resolution_clock::time_point pradzia, high_resolution_clock::time_point pabaiga) {
     auto trukme = duration_cast<milliseconds>(pabaiga - pradzia);
     return trukme;
 }
 
-void failuGeneravimas(int studentuKiekis, const string& failoPavadinimas){
-    ofstream generate (failoPavadinimas);
-    if (!generate){
+void failuGeneravimas(int studentuKiekis, const string& failoPavadinimas) {
+    ofstream generate(failoPavadinimas);
+    if (!generate) {
         cout << "Nepavyko sugeneruoti failo." << endl;
-    }else{
-        generate << left << setw(20) << "Vardas" << left << setw(20) << "Pavarde" << left << setw(5) << "ND1" << left << setw(5) << "ND2"<< left << setw(5) << "ND3"<<
-        left << setw(5) << "ND4"<< left << setw(5) << "ND5"<< left << setw(5) << "ND6"<< left << setw(5) << "ND7"<< left << setw(5) << "ND8"<< left << setw(5) << "ND9"
-        << left << setw(5) << "ND10" << left << setw(5) << "Eg." <<  endl;
-        for (int i = 0; i < studentuKiekis; i++){
-            generate << left << setw(6) << "Vardas" << left << setw(14)<< i + 1 << left << setw(7) << "Pavarde" << left << setw(13) << i + 1;
-            for (int j = 0; j < 10; j++){
-              int rand_paz = rand() % 10 + 1;
-              generate << left << setw(5) << rand_paz;
-            } 
-            int rand_eg =  rand() % 10 + 1;
-            generate <<  left << setw(5) << rand_eg << endl;
+    }
+    else {
+        generate << left << setw(20) << "Vardas" << left << setw(20) << "Pavarde" << left << setw(5) << "ND1" << left << setw(5) << "ND2" << left << setw(5) << "ND3" <<
+            left << setw(5) << "ND4" << left << setw(5) << "ND5" << left << setw(5) << "ND6" << left << setw(5) << "ND7" << left << setw(5) << "ND8" << left << setw(5) << "ND9"
+            << left << setw(5) << "ND10" << left << setw(5) << "Eg." << endl;
+        for (int i = 0; i < studentuKiekis; i++) {
+            generate << left << setw(6) << "Vardas" << left << setw(14) << i + 1 << left << setw(7) << "Pavarde" << left << setw(13) << i + 1;
+            for (int j = 0; j < 10; j++) {
+                int rand_paz = rand() % 10 + 1;
+                generate << left << setw(5) << rand_paz;
+            }
+            int rand_eg = rand() % 10 + 1;
+            generate << left << setw(5) << rand_eg << endl;
         }
         generate.close();
     }
 }
 
-void konteineriai(int studentuKiekis, vector<mok>& studentai, char a, vector<mok>& vargsiukai, vector<mok>& kietiakai){
-                        
-            if (a == 'M' || a =='m'){
-                for (int i = 0; i < studentuKiekis; i++){
-                if (studentai[i].gal_med < 5){
-                        vargsiukai.push_back(studentai[i]);
-                }else{
-                    kietiakai.push_back(studentai[i]);
-                }
-            }   
-            }else{
-                for (int i = 0; i < studentuKiekis; i++){
-                if (studentai[i].gal_vid < 5){
-                        vargsiukai.push_back(studentai[i]);
-                }else{
-                    kietiakai.push_back(studentai[i]);
-                }
-                }
-            }
-            
+void konteineriai(int studentuKiekis, vector<mok>& studentai, char a, vector<mok>& vargsiukai, vector<mok>& kietiakai) {
+    if (a == 'M' || a == 'm') {
+        auto partitionIt = partition(studentai.begin(), studentai.end(),
+            [](const mok& student) {
+                return student.gal_med < 5;
+            });
+
+        vargsiukai.insert(vargsiukai.end(), studentai.begin(), partitionIt);
+        kietiakai.insert(kietiakai.end(), partitionIt, studentai.end());
+    }
+    else {
+        auto partitionIt = partition(studentai.begin(), studentai.end(),
+            [](const mok& student) {
+                return student.gal_vid < 5;
+            });
+
+        vargsiukai.insert(vargsiukai.end(), studentai.begin(), partitionIt);
+        cout << "vargsiukai sorted" << endl;
+        kietiakai.insert(kietiakai.end(), partitionIt, studentai.end());
+        cout << "kietiakai sorted" << endl;
+    }
+
+    studentai.clear();
+    studentai.shrink_to_fit();
+    /*if (a == 'M' || a =='m'){
+    for (int i = 0; i < studentuKiekis; i++){
+    if (studentai[i].gal_med < 5){
+                 vargsiukai.push_back(studentai[i]);
+         }else{
+             kietiakai.push_back(studentai[i]);
+         }
+     }   
+     }else{
+         for (auto& student : studentai){
+             if (student.gal_vid < 5){
+                     vargsiukai.push_back(student);
+                     studentai.nd.clear();
+                     studentai[i].delete();
+             }else{
+                 kietiakai.push_back(studentai[i]);
+             }
+         }
+     }*/
+
 }
 
-void isvalymas (vector<mok>& vektorius) {
+void isvalymas(vector<mok>& vektorius) {
     for (int i = 0; i < vektorius.size(); i++) {
-    vektorius[i].nd.clear();
+        vektorius[i].nd.clear();
     }
     vektorius.clear();
 }
+void failuNuskaitymas(vector<mok>& studentai, string& failoPavadinimas){
+    ifstream file;
+    do {
+        file.open(failoPavadinimas);
+        if (!file) {
+            cout << "Nepavyko atidaryti failo." << endl;
+            cin >> failoPavadinimas;
+        }
+    } while (!file.is_open());
+    
+    int pazymys;
+    int laikinas = 0;
+    auto start = std::chrono::high_resolution_clock::now();
+    cout << "---------------------------------------------------------" << endl;
+    string line;
+    istringstream iss;
+    string grade = "";
+    getline(file, line); // nereikalingas line'as pasalinamas
+    while (getline(file, line)) {
+        iss.str(line);
+        mok laikinasMok;
+        iss >> laikinasMok.var >> laikinasMok.pav;
+        while (iss >> grade) {
+            try {
+                if (stoi(grade) >= 0 && stoi(grade) <= 10) {
+                    laikinasMok.nd.push_back(std::stoi(grade));
+                }
+            }
+            catch (std::exception& e) {
+
+            }
+        }
+        laikinasMok.eg = laikinasMok.nd.back();
+        laikinasMok.nd.pop_back();
+        studentai.push_back(laikinasMok);
+        iss.clear();
+
+    }
+    file.close();
+}
+
