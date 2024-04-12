5aec74eb335378d780092aa6ede2f1491a39fb58
LivetaKvl2 <166857419+LivetaKvl2@users.noreply.github.com>
2024-04-12 20:57:04 +0300

    Update studentai.cpp

--- 1 STRATEGIJA/Deque/studentai.cpp
+++ 1 STRATEGIJA/Deque/studentai.cpp
@@ -177,6 +177,7 @@ void isvalymas(deque<mok>& vektorius) {
     }
     vektorius.clear();
 }
+
 void failuNuskaitymas(deque<mok>& studentai, string& failoPavadinimas) {
     ifstream file;
     do {
