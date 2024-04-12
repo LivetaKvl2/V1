c5257bbbe63c77335b02b94d32091251c1d2247b
LivetaKvl2 <166857419+LivetaKvl2@users.noreply.github.com>
2024-04-12 21:00:14 +0300

    Update studentai.cpp

--- 2 STRATEGIJA/List/studentai.cpp
+++ 2 STRATEGIJA/List/studentai.cpp
@@ -1,6 +1,5 @@
 #include "studentai.h"
 
-
 void ivedimas(list<mok>& stud) {
     mok naujas_stud;
     cout << "Iveskite studento varda ir pavarde:" << endl;
@@ -56,7 +55,6 @@ void meniu(int& antrasPasirinkimas) {
 }
 
 void calculateResults(list<mok>& stud) {
-
     for (auto i = stud.begin(); i != stud.end(); i++) {
         double sum = accumulate(i->nd.begin(), i->nd.end(), 0.0);
         double vid = sum / i->nd.size();
@@ -87,7 +85,6 @@ char rikiavimoklausimas() {
     return a;
 }
 void isvedimas(list<mok>& stud, ostream& os, char a) {
-
     if (a == 'V' || a == 'v') {
         os << left << setw(20) << "Pavarde" << setw(20) << "Vardas" << setw(20) << "Galutinis (Vid.)" << endl;
         os << "--------------------------------------------------------------" << endl;
@@ -119,12 +116,15 @@ void meniuAntras(int& antrasPasirinkimas) {
 bool pagalVarda(const mok& a, const mok& b) {
     return stoi(a.var.substr(6, 1)) < stoi(b.var.substr(6, 1));
 }
+
 bool pagalPavarde(const mok& a, const mok& b) {
     return stoi(a.pav.substr(7, 1)) < stoi(b.pav.substr(7, 1));
 }
+
 bool pagalMediana(const mok& a, const mok& b) {
     return a.gal_med < b.gal_med;
 }
+
 bool pagalVidurki(const mok& a, const mok& b) {
     return a.gal_vid < b.gal_vid;
 }
